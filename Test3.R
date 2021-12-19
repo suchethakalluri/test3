19fafe30ee0c73a6c6b046821a4a16e22ef0f6f2
Suchetha Kalluri <suchetha.kalluri@gmail.com>
2021-12-18 22:03:21 -0800

    Commit 3 because initial commit and commit 2 did not work.

--- Test3.R
+++ Test3.R
@@ -3,3 +3,5 @@ print("this file was created within RStudio")
 print("and it now lives on GitHub")
 
 print("making changes to code because initial push did not work")
+
+print("trying to push test 3 for the 3rd time")
